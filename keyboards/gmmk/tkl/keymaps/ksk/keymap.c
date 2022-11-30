0f2ffe6998b0c4d8965feaf3628f83a3596eb80e
voidedWarranties <hi+github@voidedxd.xyz>
2022-11-29 21:16:19 -0800

    Adjust settings
    - Made the FKEYS layer key less terrible on the pinky (hopefully)
    - Adjusted mouse key settings to allow for more precise movement

--- keyboards/gmmk/tkl/keymaps/ksk/keymap.c
+++ keyboards/gmmk/tkl/keymaps/ksk/keymap.c
@@ -22,7 +22,7 @@ enum layers {
 #define CUT C(KC_X)
 
 #define LYR_NAV LT(_NAV, KC_SCLN)
-#define LYR_FKS LT(_FKEYS, KC_SLSH)
+#define LYR_FKS LT(_FKEYS, KC_QUOT)
 #define LYR_SMB LT(_SYMBOL, KC_SPC)
 #define LYR_ADJ MO(_ADJUST)
 
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ansi(   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,        XXXXXXX,    XXXXXXX,
                              XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,        XXXXXXX,    XXXXXXX,
                              KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,           KC_P,           KC_BSPC,    _______,
-                             ESC_CTL,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,           LYR_NAV,        KC_QUOT,    _______,
-                             KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,         LYR_FKS,        KC_RSFT,    KC_ENT,
+                             ESC_CTL,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,           LYR_NAV,        LYR_FKS,    _______,
+                             KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,         KC_SLSH,        KC_RSFT,    KC_ENT,
                              KC_LCTL,    KC_LGUI,    KC_LALT,    LYR_SMB,    KC_RALT,    LYR_ADJ,    KC_APP,     KC_RCTL,    XXXXXXX,    XXXXXXX,        XXXXXXX,        XXXXXXX,    XXXXXXX,
                              XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX),
 
